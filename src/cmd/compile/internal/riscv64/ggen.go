5da9dce935d183e193dda7007f13639caf7e979a
Tobias Klauser <tklauser@distanz.ch>
2019-01-10 14:33:46 +0100

    cmd/compile/internal/riscv64: adjust ginsnop
    Fixes build after merging CL 152537
    
    Change-Id: I3f3c690bd5a82d11dd009c6be422e7cf2d2b7ed1

--- src/cmd/compile/internal/riscv64/ggen.go
+++ src/cmd/compile/internal/riscv64/ggen.go
@@ -79,10 +79,11 @@ func zeroAuto(pp *gc.Progs, n *gc.Node) {
 	// TODO(tklauser)
 }
 
-func ginsnop(pp *gc.Progs) {
+func ginsnop(pp *gc.Progs) *obj.Prog {
 	// Hardware nop is ADD $0, ZERO
 	p := pp.Prog(riscv64.AADD)
 	p.From.Type = obj.TYPE_CONST
 	p.SetFrom3(obj.Addr{Type: obj.TYPE_REG, Reg: riscv64.REG_ZERO})
 	p.To = *p.GetFrom3()
+	return p
 }
